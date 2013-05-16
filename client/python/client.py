50d65abf1e35d287bee93838fe0f77708a2758d4
Greg Berger <berger.gregory@gmail.com>
2013-05-16 16:31:10 +0200

    a simple web server to process tests

--- client/python/client.py
+++ client/python/client.py
@@ -4,18 +4,27 @@ __author__ = 'Greg Berger'
 import socket
 import gpioProcess
 
-IP = "188.165.193.200"
-PORT = 33333
 
-BUFFER_SIZE = 20
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.connect((IP,PORT))
-print "Connected"
-while True:
-    data = s.recv(BUFFER_SIZE)
-    gpioProcess.processReceivedData(data)
+def tcp_serve():
+    IP = "188.165.193.200"
+    PORT = 33333
+    BUFFER_SIZE = 20
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((IP,PORT))
+    print "Connected"
+    while True:
+        data = s.recv(BUFFER_SIZE)
+        gpioProcess.processReceivedData(data)
 
-s.close()
+    s.close()
+    webServer.StreamHandler.close()
+
+
+tcp_serve()
+
+
+
+
 
